14918488be098626d0416ebef0379f3387105c6c
Cameron Little <cameron@camlittle.com>
2014-03-16 20:18:05 -0700

    Added ISO 101226 padding

--- iso101226.go
+++ iso101226.go
@@ -1,24 +1,31 @@
 package crypto_padding
 
 import (
-    "bytes"
+    //"bytes"
+    "crypto/rand"
     "errors"
     "fmt"
 )
 
-type AnsiX923 struct {}
+type ISO101226 struct {}
 
-func (padding AnsiX923) Pad(data []byte, blockSize int) (output []byte, err error) {
+func (padding ISO101226) Pad(data []byte, blockSize int) (output []byte, err error) {
     if blockSize < 1 || blockSize >= 256 {
         return output, errors.New(fmt.Sprintf("blocksize is out of bounds: %v", blockSize))
     }
     var paddingBytes = padSize(len(data), blockSize)
-    paddingSlice := append(bytes.Repeat([]byte{byte(0)}, paddingBytes - 1), byte(paddingBytes))
+    paddingSlice := make([]byte, paddingBytes - 1)
+    _, err = rand.Read(paddingSlice)
+    if err != nil {
+        return output, err
+    }
+    paddingSlice = append(paddingSlice, byte(paddingBytes))
+    fmt.Println(paddingSlice)
     output = append(data, paddingSlice...)
     return output, nil
 }
 
-func (padding AnsiX923) Unpad(data []byte, blockSize int) (output []byte, err error) {
+func (padding ISO101226) Unpad(data []byte, blockSize int) (output []byte, err error) {
     var dataLen = len(data)
     if dataLen % blockSize != 0 {
         return output, errors.New("data's length isn't a multiple of blockSize")
@@ -26,12 +33,6 @@ func (padding AnsiX923) Unpad(data []byte, blockSize int) (output []byte, err er
     var paddingBytes = int(data[dataLen - 1])
     if paddingBytes > blockSize || paddingBytes <= 0 {
         return output, errors.New(fmt.Sprintf("invalid padding found: %v", paddingBytes))
-    }
-    var pad = data[dataLen - paddingBytes:dataLen - 2]
-    for _, v := range pad {
-        if int(v) != 0 {
-            return output, errors.New("invalid padding found")
-        }
     }
     output = data[0:dataLen - paddingBytes]
     return output, nil
